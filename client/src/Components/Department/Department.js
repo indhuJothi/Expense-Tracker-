7cd586726aa1655b19b5992ee26e573cb2c7dd6d
indhuJothi <indhu.palanivel@aspiresys.com>
2021-12-15 18:07:03 +0530

    Complted-add-edit-delete-of-Department

--- client/src/Components/Department/Department.js
+++ client/src/Components/Department/Department.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Header from "../Header/Header";
-
+import { DepartmentTable } from "./DepartmentTable";
 class Department extends React.Component{
     render(){
         return(
-            <Header/>
+            <>
+              <Header/>
+              <DepartmentTable/>
+            </>
+          
+
         )
     }
 }
--- client/src/index.js
+++ client/src/index.js
@@ -5,11 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Department from '../src/Components/Department/Department'
+import {BrowserRouter as Router} from 'react-router-dom'
+import 'antd/dist/antd.css'
 
 ReactDOM.render(
   <React.StrictMode>
+    <Router>
    {/* <App/> */}
    <Department/>
+   </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
